58406b35d2a52fafec1d5bcf32a13b10ff7d1986
Matthew Alner <matthew.alner@senseye.io>
2019-05-27 17:02:30 +0100

    now looping through json for easy config / addition

--- built-with-segment.sh
+++ built-with-segment.sh
@@ -8,19 +8,26 @@ built_with() {
     # bail out if there is no package.json
     if [ ! -f "./package.json" ]; then echo "" && exit 0; fi
 
-    # bail out if there is no package.json
+    # bail out if 'jq' isn't installed
     if ! (hash jq &> /dev/null) ; then echo "install jq" && exit 0; fi
 
     # merge together dependencies and devDependencies from package.json
     ALL_DEPS=$(jq < package.json '[.dependencies,.devDependencies] | .[0] + .[1]')
     OUTPUT=""
-    # echo "$opt" | tr -d '"'
-    if [ "$(echo "$ALL_DEPS" | jq 'has("@angular/core")')" = 'true' ]; then OUTPUT="$OUTPUT \ufbb0"; fi
-    if [ "$(echo "$ALL_DEPS" | jq 'has("typescript")')" = 'true' ]; then OUTPUT="$OUTPUT \ue628"; fi
-    if [ "$(echo "$ALL_DEPS" | jq 'has("grunt")')" = 'true' ]; then OUTPUT="$OUTPUT \ue611"; fi
-    if [ "$(echo "$ALL_DEPS" | jq 'has("gulp")')" = 'true' ]; then OUTPUT="$OUTPUT \ue610"; fi
-    if [ "$(echo "$ALL_DEPS" | jq 'has("react")')" = 'true' ]; then OUTPUT="$OUTPUT \ue625"; fi
-    if [ "$(echo "$ALL_DEPS" | jq 'has("vue")')" = 'true' ]; then OUTPUT="$OUTPUT \ufd42"; fi
-    if [ "$(echo "$ALL_DEPS" | jq 'has("webpack")')" = 'true' ]; then OUTPUT="$OUTPUT \ufc29"; fi
+
+    for row in $(jq < built-with-config.json -r '.[] | @base64'); do
+        _jq() {
+            echo ${row} | base64 --decode | jq -r ${1}
+        }
+
+        built_with_check_package_json "$(_jq '.name')" "$(_jq '.icon')"
+    done
+
     echo "$OUTPUT"
+}
+
+built_with_check_package_json() {
+    local keyToCheck=${1}
+    local icon=${2}
+    if [ "$(echo "$ALL_DEPS" | jq --arg KE "$keyToCheck" 'has($KE)')" = 'true' ]; then OUTPUT="$OUTPUT $icon"; fi
 }
