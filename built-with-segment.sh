c0a081cac24fcbaef6ef2091db2233d1727d847d
Matthew Alner <matthew.alner@senseye.io>
2019-06-01 10:34:33 +0100

    added new checks

--- built-with-segment.sh
+++ built-with-segment.sh
@@ -14,47 +14,59 @@ built_with() {
     # merge together dependencies and devDependencies from package.json
     ALL_DEPS=$(jq < package.json '[.dependencies,.devDependencies] | .[0] + .[1]')
     OUTPUT=""
+    ROW_OUTPUT=""
 
-    for row in $(jq < built-with-config.json -r '.[] | @base64'); do
+    for row in $(jq < "$HOME"/built-with-config.json -r '.[] | @base64'); do
         _jq() {
             echo "${row}" | base64 --decode | jq -r "${1}"
         }
 
-        if [ "$(echo "${row}" | base64 --decode | jq 'has("packageJSONKey")')" = 'true' ]; then 
-            built_with_check_package_json "$(_jq '.packageJSONKey')" "$(_jq '.icon')" "$(_jq '.showIcon')" "$(_jq '.showVersion')"
+        if [ "$(echo "${row}" | base64 --decode | jq 'has("keyExists")')" = 'true' ]; then 
+            built_with_check_package_json_key_exists "$(_jq '.keyExists')" "$(_jq '.icon')" "$(_jq '.showIcon')" "$(_jq '.showVersion')"
         fi
-        if [ "$(echo "${row}" | base64 --decode | jq 'has("fileToCheckFor")')" = 'true' ]; then 
-            built_with_check_for_existance_of_file "$(_jq '.fileToCheckFor')" "$(_jq '.icon')" "$(_jq '.showIcon')" "$(_jq '.showVersion')"
+        if [ "$(echo "${row}" | base64 --decode | jq 'has("fileExists")')" = 'true' ]; then 
+            built_with_check_file_exists "$(_jq '.fileExists')" "$(_jq '.icon')" "$(_jq '.showIcon')" "$(_jq '.showVersion')"
         fi
+        if [ "$(echo "${row}" | base64 --decode | jq 'has("keyAbsent")')" = 'true' ]; then 
+            built_with_check_package_json_key_absent "$(_jq '.keyAbsent')"
+        fi
+
+        OUTPUT="$OUTPUT$ROW_OUTPUT"
+        ROW_OUTPUT=""
     done
 
     echo "$OUTPUT"
 }
 
-built_with_check_package_json() {
+built_with_check_package_json_key_exists() {
     local keyToCheck=${1}
     local icon=${2}
     local showIcon=${3}
     local showVersion=${4}
 
     if [ "$(echo "$ALL_DEPS" | jq --arg KEY "$keyToCheck" 'has($KEY)')" = 'true' ]; then
-            local versionNumber
-            versionNumber="$(echo "$ALL_DEPS" | jq --arg KEY "$keyToCheck" '.[$KEY'] | tr -d '"')"
-            if [ "$showIcon" = 'true' ]; then OUTPUT="$OUTPUT $icon"; fi
-            if [ "$showVersion" = 'true' ]; then OUTPUT="$OUTPUT $versionNumber"; fi
+        local versionNumber
+        versionNumber="$(echo "$ALL_DEPS" | jq --arg KEY "$keyToCheck" '.[$KEY'] | tr -d '"')"
+        if [ "$showIcon" = 'true' ]; then ROW_OUTPUT="$ROW_OUTPUT $icon"; fi
+        if [ "$showVersion" = 'true' ]; then ROW_OUTPUT="$ROW_OUTPUT $versionNumber"; fi
     fi
 }
 
-built_with_check_for_existance_of_file() {
+built_with_check_file_exists() {
     local fileToCheck=${1}
     local icon=${2}
     local showIcon=${3}
     local showVersion=${4}
 
     if [ -f "$fileToCheck" ]; then
-            local versionNumber
-            versionNumber="$(echo "$ALL_DEPS" | jq --arg KEY "$keyToCheck" '.[$KEY'] | tr -d '"')"
-            if [ "$showIcon" = 'true' ]; then OUTPUT="$OUTPUT $icon"; fi
-            if [ "$showVersion" = 'true' ]; then OUTPUT="$OUTPUT $versionNumber"; fi
+        if [ "$showIcon" = 'true' ]; then ROW_OUTPUT="$ROW_OUTPUT $icon"; fi
+    fi
+}
+
+built_with_check_package_json_key_absent() {
+    local keyToCheck=${1}
+
+    if [ "$(echo "$ALL_DEPS" | jq --arg KEY "$keyToCheck" 'has($KEY)')" = 'true' ]; then
+        ROW_OUTPUT="";
     fi
 }
